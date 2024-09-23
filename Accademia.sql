307aa9d5ea33492232905da4bf9fbde5ddaf0a46
PhilipVicari <philipvicari9@gmail.com>
2024-09-23 10:25:45 +0200

    Accademia compito

--- Accademia.sql
+++ Accademia.sql
@@ -1,28 +1,28 @@
-create database Accademia
+begin transaction;
 
-create type Strutturato as enum ('Ricercatore, Professore Associato, Professore Ordinario')
-create type LavoroProgetto as enum ('Ricerca e Sviluppo, Dimostrazione, Management, Altro')
-create type LavoroNonProgettuale as enum ('Didattica, Ricerca, Missione, Incontro Dipartimentale, Incontro Accademico, Altro')
-create type CausaAssenza as enum ('Chiusura Universitaria, Maternita, Malattia')
-create domain PosInteger as integer 
+create type Strutturato as enum ('Ricercatore', 'Professore Associato', 'Professore Ordinario');
+create type LavoroProgetto as enum ('Ricerca e Sviluppo', 'Dimostrazione', 'Management, Altro');
+create type LavoroNonProgettuale as enum ('Didattica', 'Ricerca', 'Missione', 'Incontro Dipartimentale', 'Incontro Accademico', 'Altro');
+create type CausaAssenza as enum ('Chiusura Universitaria', 'Maternita, Malattia');
+create domain PosInteger as integer
     default 0
-    CHECK (value>=0)
-create domain StringaM as varchar(100)
-create domain NumeroOre as integer 
+    CHECK (value>=0);
+create domain StringaM as varchar(100);
+create domain NumeroOre as integer
     default 0
-    CHECK (value >= 0 and value <= 8)
-create domain Denaro as real 
+    CHECK (value >= 0 and value <= 8);
+create domain Denaro as real
     default 0
-    CHECK (value >= 0)
+    CHECK (value >= 0);
 
 
 create table Persona (
   id PosInteger not null,
   nome StringaM not null,
   cognome StringaM not null,
-  pos_strutturato not null,
+  posizione strutturato not null,
   stipendio Denaro not null,
-  PRIMARY KEY (id)  
+  PRIMARY KEY (id)
 );
 
 create table Progetto (
@@ -81,38 +81,66 @@ create table Assenza (
   FOREIGN KEY (persona) references Persona(id)  
 );
 
---Quali sono i cognomi distinti di tutti gli strutturati?
-SELECT distinct Persona.cognome
-from Persona
-where Persona.pos_strutturato = 'Strutturato'
---Quali sono i Ricercatori (con nome e cognome)?
-SELECT Persona.nome, Persona.cognome
-from Persona
-where Persona.pos_strutturato = 'Ricercatore'
---Quali sono i Professori Associati il cui cognome comincia con la lettera ‘V’ ?
-SELECT Persona.nome, Persona.cognome
-from Persona
-where Persona.pos_strutturato = 'Professore Associato' and cognome like 'V%'
---Quali sono i Professori (sia Associati che Ordinari) il cui cognome comincia con la lettera ‘V’ ?
-SELECT Persona.nome, Persona.cognome
-from Persona
-where Persona.pos_strutturato = 'Professore' and cognome like 'V%'
---Quali sono i Progetti già terminati alla data odierna?
 
---Quali sono i nomi di tutti i Progetti ordinati in ordine crescente di data di inizio?
-SELECT Progetto.nome
-from Progetto
-ORDER BY inizio ASC
---Quali sono i nomi dei WP ordinati in ordine crescente (per nome)?
-SELECT WP.nome
-from WP
-ORDER BY nome ASC
---Quali sono (distinte) le cause di assenza di tutti gli strutturati?
 
---Quali sono (distinte) le tipologie di attività di progetto di tutti gli strutturati?
 
--- Quali sono i giorni distinti nei quali del personale ha effettuato attività non progettuali di tipo ‘Didattica’ ? Dare il risultato in ordine crescente.
-SELECT distinct AttivitaNonProgettuale.giorno
-from AttivitaNonProgettuale
-where AttivitaNonProgettuale.tipo = 'Didattica'
-ORDER BY giorno ASC
+-- Definire in SQL le seguenti interrogazioni, in cui si chiedono tutti risultati distinti:
+-- 1. Quali sono il nome, la data di inizio e la data di fine dei WP del progetto di nome
+-- ‘Pegasus’ ?
+SELECT  WP.nome, WP.inizio, WP.fine
+from WP, Progetto
+where Progetto.nome = 'Pegasus'
+
+-- 2. Quali sono il nome, il cognome e la posizione degli strutturati che hanno almeno
+-- una attività nel progetto ‘Pegasus’, ordinati per cognome decrescente?
+SELECT Persona.nome, Persona.cognome, Persona.posizione
+from Persona, Progetto
+where Progetto.nome = 'Pegasus'
+ORDER by desc
+
+-- 3. Quali sono il nome, il cognome e la posizione degli strutturati che hanno più di
+-- una attività nel progetto ‘Pegasus’ ?
+SELECT Persona.nome, Persona.cognome, Persona.posizione
+from Persona, Progetto
+where Progetto.nome = 'Pegasus'
+
+-- 4. Quali sono il nome, il cognome e la posizione dei Professori Ordinari che hanno
+-- fatto almeno una assenza per malattia?
+SELECT Persona.nome, Persona.cognome, Persona.posizione, Assenza.tipo
+from Persona, Progetto, Assenza
+where posizione = 'Professore Ordinario' and Assenza.tipo = 'Malattia'
+
+-- 5. Quali sono il nome, il cognome e la posizione dei Professori Ordinari che hanno
+-- fatto più di una assenza per malattia?
+SELECT Persona.nome, Persona.cognome, Persona.posizione, Assenza.tipo
+from Persona, Progetto, Assenza
+where posizione = 'Professore Ordinario' and Assenza.tipo = 'Malattia'
+
+-- 6. Quali sono il nome, il cognome e la posizione dei Ricercatori che hanno almeno
+-- un impegno per didattica?
+SELECT Persona.nome, Persona.cognome, Persona.posizione
+from Persona, Progetto
+where posizione = 'Ricercatore' and 
+
+-- 7. Quali sono il nome, il cognome e la posizione dei Ricercatori che hanno più di un
+-- impegno per didattica?
+SELECT Persona.nome, Persona.cognome, Persona.posizione
+from Persona, Progetto
+where posizione = 'Ricercatore' and
+
+-- 8. Quali sono il nome e il cognome degli strutturati che nello stesso giorno hanno sia
+-- attività progettuali che attività non progettuali?
+
+-- 9. Quali sono il nome e il cognome degli strutturati che nello stesso giorno hanno sia
+-- attività progettuali che attività non progettuali? Si richiede anche di proiettare il
+-- giorno, il nome del progetto, il tipo di attività non progettuali e la durata in ore di
+-- entrambe le attività.
+
+-- 10. Quali sono il nome e il cognome degli strutturati che nello stesso giorno sono
+-- assenti e hanno attività progettuali?
+
+-- 11. Quali sono il nome e il cognome degli strutturati che nello stesso giorno sono
+-- assenti e hanno attività progettuali? Si richiede anche di proiettare il giorno, il
+-- nome del progetto, la causa di assenza e la durata in ore della attività progettuale.
+
+-- 12. Quali sono i WP che hanno lo stesso nome, ma appartengono a progetti diversi?
