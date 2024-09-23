12f8405904a2204118e67309ad59fe8588685f10
PhilipVicari <philipvicari9@gmail.com>
2024-09-23 12:59:36 +0200

    23/09/24

--- Cielo.sql
+++ Cielo.sql
@@ -7,38 +7,41 @@ create domain StringaM as varchar(100);
 create domain CodiATA as char(3);
 
 create table Volo(
-    codice: PosInteger,
-    comp: StringaM,
-    durataMinuti: PosInteger,
-    PRIMARY KEY (codice)
-    FOREIGN KEY comp references Compagnia(nome)
+    codice PosInteger not null,
+    comp StringaM not null,
+    durataMinuti PosInteger not null,
+    PRIMARY KEY (codice),
+    FOREIGN KEY comp references Compagnia(nome),
     FOREIGN KEY (codice, comp)references ArrPart(codice, comp)
 );
 create table ArrPart(
-    codice: PosInteger,
-    comp: StringaM,
-    arrivo: CodiATA,
-    partenza: CodiATA,
-    PRIMARY KEY (codice)
-    FOREIGN KEY (codice, comp)references Volo(codice, comp)
-    FOREIGN KEY arrivo references Aereoporto(codice)
-    FOREIGN KEY partenza references Aereoporto(codice)
+    codice PosInteger not null,
+    comp StringaM not null,
+    arrivo CodiATA not null,
+    partenza CodiATA not null,
+    PRIMARY KEY (codice),
+    FOREIGN KEY (codice, comp)references Volo(codice, comp),
+    FOREIGN KEY (arrivo) references Aereoporto(codice),
+    FOREIGN KEY (partenza) references Aereoporto(codice)
 );
 create table Aereoporto(
-    codice: CodiATA,
-    nome: StringaM,
-    PRIMARY KEY (codice)
-    FOREIGN KEY codice references LuogoAereoporto(Aereoporto)
+    codice CodiATA not null,
+    nome StringaM not null,
+    PRIMARY KEY (codice),
+    FOREIGN KEY (codice) references LuogoAereoporto(Aereoporto)
 );
 create table LuogoAereoporto(
-    aereoporto: CodiATA, 
-    citta: StringaM,
-    nazione: StringaM,
-    PRIMARY KEY (aereoporto)
-    FOREIGN KEY aereoporto references Aereoporto(codice)
+    aereoporto CodiATA not null,
+    citta StringaM not null,
+    nazione StringaM not null,
+    PRIMARY KEY (aereoporto),
+    FOREIGN KEY (aereoporto) references Aereoporto(codice)
 );
 create table Compagnia(
-    nome: StringaM,
-    annoFondazione: PosInteger,
+    nome StringaM not null,
+    annoFondazione PosInteger null,
     PRIMARY KEY(nome)
 );
+
+
+commit
