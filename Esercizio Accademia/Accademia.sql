90dfbbbca870acfdaab5e18599e1206fe63f29f9
PhilipVicari <philipvicari9@gmail.com>
2024-10-07 11:52:10 +0200

    agg esercizi

--- Esercizio Accademia/Accademia.sql
+++ Esercizio Accademia/Accademia.sql
@@ -83,99 +83,7 @@ create table Assenza (
 
 
 
-
--- Definire in SQL le seguenti interrogazioni, in cui si chiedono tutti risultati distinti:
--- 1. Quali sono il nome, la data di inizio e la data di fine dei WP del progetto di nome
--- ‘Pegasus’ ?
-SELECT  WP.nome, WP.inizio, WP.fine
-from WP, Progetto
-where WP.progetto = Progetto.id and Progetto.nome = 'Pegasus'
--- 2. Quali sono il nome, il cognome e la posizione degli strutturati che hanno almeno
--- una attività nel progetto ‘Pegasus’, ordinati per cognome decrescente?
-SELECT distinct s.nome, s.cognome, s.posizione
-from AttivitaProgetto a, Persona s, Progetto p
-where a.persona = s.id 
-    and a.progetto = p.id 
-    and p.nome = 'Pegasus'
-ORDER by s.cognome desc
-
--- 3. Quali sono il nome, il cognome e la posizione degli strutturati che hanno più di
--- una attività nel progetto ‘Pegasus’ ?
-SELECT *
-from AttivitaProgetto a1, AttivitaProgetto a2, progetto p,
-where a1.id
--- 4. Quali sono il nome, il cognome e la posizione dei Professori Ordinari che hanno
--- fatto almeno una assenza per malattia?
-select distinct pe.id, pe.nome, pe.cognome
-from Persona pe
-join Assenza a ON pe.id = a.persona
-where pe.posizione= 'Professore Ordinario'
-	and a.tipo = 'Malattia'
-
--- 5. Quali sono il nome, il cognome e la posizione dei Professori Ordinari che hanno
--- fatto più di una assenza per malattia?
-SELECT p.nome, p.cognome, p.posizione
-FROM Persona p
-JOIN Assenza a ON p.id = a.persona
-WHERE p.posizione = 'Professore Ordinario' AND a.tipo = 'Malattia'
-GROUP BY p.id, p.nome, p.cognome, p.posizione
-HAVING COUNT(*) > 1;
--- 6. Quali sono il nome, il cognome e la posizione dei Ricercatori che hanno almeno
--- un impegno per didattica?
-select distinct pe.id, pe.nome, pe.cognome
-from Persona pe
-join AttivitaNonProgettuale atn ON pe.id = atn.persona
-where pe.posizione= 'Ricercatore'
-	and atn.tipo = 'Didattica'
--- 7. Quali sono il nome, il cognome e la posizione dei Ricercatori che hanno più di un
--- impegno per didattica?
-SELECT p.nome, p.cognome, p.posizione
-FROM Persona p
-JOIN AttivitaNonProgettuale atn ON p.id = atn.persona
-WHERE p.posizione = 'Ricercatore' AND atn.tipo = 'Didattica'
-GROUP BY p.id, p.nome, p.cognome, p.posizione
-HAVING COUNT(*) > 1;
--- 8. Quali sono il nome e il cognome degli strutturati che nello stesso giorno hanno sia
--- attività progettuali che attività non progettuali?
-SELECT DISTINCT pe id, pe.nome, pe.cognome
-FROM Persona pe
-JOIN AttivitaProgetto atp ON pe.id = ap.persona
-JOIN AttivitaNonProgettuale atn ON pe.id = atn.persona AND atp.giorno = atn.giorno;
-
--- 9. Quali sono il nome e il cognome degli strutturati che nello stesso giorno hanno sia
--- attività progettuali che attività non progettuali? Si richiede anche di proiettare il
--- giorno, il nome del progetto, il tipo di attività non progettuali e la durata in ore di
--- entrambe le attività.
-
-SELECT p.nome, p.cognome, ap.giorno, pr.nome, anp.tipo, ap.oreDurata, anp.oreDurata
-FROM Persona p
-JOIN AttivitaProgetto ap ON p.id = ap.persona
-JOIN AttivitaNonProgettuale anp ON p.id = anp.persona AND ap.giorno = anp.giorno
-JOIN Progetto pr ON ap.progetto = pr.id
-
--- 10. Quali sono il nome e il cognome degli strutturati che nello stesso giorno sono
--- assenti e hanno attività progettuali?
-SELECT DISTINCT pe.id, pe.nome, pe.cognome
-FROM Persona pe
-JOIN AttivitaProgetto atp ON pe.id = atp.persona
-join Assenza a on pe.id = a.persona and atp.giorno = a.giorno
-
--- 11. Quali sono il nome e il cognome degli strutturati che nello stesso giorno sono
--- assenti e hanno attività progettuali? Si richiede anche di proiettare il giorno, il
--- nome del progetto, la causa di assenza e la durata in ore della attività progettuale.
-SELECT pe.nome, pe.cognome, atp.giorno, pr.nome, a.tipo, atp.oreDurata
-FROM Persona pe
-JOIN AttivitaProgetto atp ON pe.id = atp.persona
-JOIN Progetto pr ON atp.progetto = pr.id
-join Assenza a on pe.id = a.persona and atp.giorno = a.giorno
--- 12. Quali sono i WP che hanno lo stesso nome, ma appartengono a progetti diversi?
-select WP.nome
-from WP
-join Progetto pr on pr.id = WP.progetto
-where WP.nome =
-
-
---Accademia6
+--ACCADEMIA 6 -----------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- 1. Quanti sono gli strutturati di ogni fascia?
 SELECT posizione, count(*)
@@ -187,9 +95,9 @@ from persona p
 where p.stipendio >= 40000
 -- 3. Quanti sono i progetti già finiti che superano il budget di 50000? ****
 select count(*)
-from progetto pr, persona p
-where p.stipendio >= 50000
-  and pr.fine = "Current Date"
+from progetto pr
+where pr.budget >= 50000
+  and pr.fine < Current_date
 -- 4. Qual è la media, il massimo e il minimo delle ore delle attività relative al progetto
 -- ‘Pegasus’ ?
 select avg(oreDurata), max(oreDurata), min(oreDurata)
@@ -218,5 +126,118 @@ select avg(stipendio), max(stipendio), min(stipendio)
 from Persona p
 group by posizione
 -- 9. Quante ore ‘Ginevra Riva’ ha dedicato ad ogni progetto nel quale ha lavorato?
+SELECT ap.progetto, pr.nome, sum(ap.oreDurata)
+FROM Persona p
+JOIN AttivitaProgetto ap on ap.persona = p.id
+JOIN Progetto pr ON ap.progetto = pr.id
+WHERE p.nome = 'Ginevra' and p.cognome = 'Riva'
+GROUP by ap.id, pr.nome
 -- 10. Qual è il nome dei progetti su cui lavorano più di due strutturati?
+SELECT pr.nome, COUNT(DISTINCT ap.persona)
+FROM Progetto pr
+JOIN AttivitaProgetto ap on ap.progetto = pr.id
+JOIN Persona p on p.id = ap.persona 
+WHERE p.posizione IN ('Professore Associato', 'Professore Ordinario')
+GROUP by pr.nome
+HAVING COUNT (DISTINCT ap.persona) > 1
 -- 11. Quali sono i professori associati che hanno lavorato su più di un progetto?
+SELECT p.id, p.nome, p.cognome
+FROM Persona p
+JOIN AttivitaProgetto ap on ap.persona = p.id
+where p.posizione = 'Professore Associato'
+GROUP by p.nome, p.cognome, p.id
+
+
+
+--ACCADEMIA 5
+-- -- Definire in SQL le seguenti interrogazioni, in cui si chiedono tutti risultati distinti:
+-- -- 1. Quali sono il nome, la data di inizio e la data di fine dei WP del progetto di nome
+-- -- ‘Pegasus’ ?
+-- SELECT  WP.nome, WP.inizio, WP.fine
+-- from WP, Progetto
+-- where WP.progetto = Progetto.id and Progetto.nome = 'Pegasus'
+-- -- 2. Quali sono il nome, il cognome e la posizione degli strutturati che hanno almeno
+-- -- una attività nel progetto ‘Pegasus’, ordinati per cognome decrescente?
+-- SELECT distinct s.nome, s.cognome, s.posizione
+-- from AttivitaProgetto a, Persona s, Progetto p
+-- where a.persona = s.id 
+--     and a.progetto = p.id 
+--     and p.nome = 'Pegasus'
+-- ORDER by s.cognome desc
+
+-- -- 3. Quali sono il nome, il cognome e la posizione degli strutturati che hanno più di
+-- -- una attività nel progetto ‘Pegasus’ ?
+-- SELECT *
+-- from AttivitaProgetto a1, AttivitaProgetto a2, progetto p,
+-- where a1.id
+-- -- 4. Quali sono il nome, il cognome e la posizione dei Professori Ordinari che hanno
+-- -- fatto almeno una assenza per malattia?
+-- select distinct pe.id, pe.nome, pe.cognome
+-- from Persona pe
+-- join Assenza a ON pe.id = a.persona
+-- where pe.posizione= 'Professore Ordinario'
+-- 	and a.tipo = 'Malattia'
+
+-- -- 5. Quali sono il nome, il cognome e la posizione dei Professori Ordinari che hanno
+-- -- fatto più di una assenza per malattia?
+-- SELECT p.nome, p.cognome, p.posizione
+-- FROM Persona p
+-- JOIN Assenza a ON p.id = a.persona
+-- WHERE p.posizione = 'Professore Ordinario' AND a.tipo = 'Malattia'
+-- GROUP BY p.id, p.nome, p.cognome, p.posizione
+-- HAVING COUNT(*) > 1;
+-- -- 6. Quali sono il nome, il cognome e la posizione dei Ricercatori che hanno almeno
+-- -- un impegno per didattica?
+-- select distinct pe.id, pe.nome, pe.cognome
+-- from Persona pe
+-- join AttivitaNonProgettuale atn ON pe.id = atn.persona
+-- where pe.posizione= 'Ricercatore'
+-- 	and atn.tipo = 'Didattica'
+-- -- 7. Quali sono il nome, il cognome e la posizione dei Ricercatori che hanno più di un
+-- -- impegno per didattica?
+-- SELECT p.nome, p.cognome, p.posizione
+-- FROM Persona p
+-- JOIN AttivitaNonProgettuale atn ON p.id = atn.persona
+-- WHERE p.posizione = 'Ricercatore' AND atn.tipo = 'Didattica'
+-- GROUP BY p.id, p.nome, p.cognome, p.posizione
+-- HAVING COUNT(*) > 1;
+-- -- 8. Quali sono il nome e il cognome degli strutturati che nello stesso giorno hanno sia
+-- -- attività progettuali che attività non progettuali?
+-- SELECT DISTINCT pe id, pe.nome, pe.cognome
+-- FROM Persona pe
+-- JOIN AttivitaProgetto atp ON pe.id = ap.persona
+-- JOIN AttivitaNonProgettuale atn ON pe.id = atn.persona AND atp.giorno = atn.giorno;
+
+-- -- 9. Quali sono il nome e il cognome degli strutturati che nello stesso giorno hanno sia
+-- -- attività progettuali che attività non progettuali? Si richiede anche di proiettare il
+-- -- giorno, il nome del progetto, il tipo di attività non progettuali e la durata in ore di
+-- -- entrambe le attività.
+
+-- SELECT p.nome, p.cognome, ap.giorno, pr.nome, anp.tipo, ap.oreDurata, anp.oreDurata
+-- FROM Persona p
+-- JOIN AttivitaProgetto ap ON p.id = ap.persona
+-- JOIN AttivitaNonProgettuale anp ON p.id = anp.persona AND ap.giorno = anp.giorno
+-- JOIN Progetto pr ON ap.progetto = pr.id
+
+-- -- 10. Quali sono il nome e il cognome degli strutturati che nello stesso giorno sono
+-- -- assenti e hanno attività progettuali?
+-- SELECT DISTINCT pe.id, pe.nome, pe.cognome
+-- FROM Persona pe
+-- JOIN AttivitaProgetto atp ON pe.id = atp.persona
+-- join Assenza a on pe.id = a.persona and atp.giorno = a.giorno
+
+-- -- 11. Quali sono il nome e il cognome degli strutturati che nello stesso giorno sono
+-- -- assenti e hanno attività progettuali? Si richiede anche di proiettare il giorno, il
+-- -- nome del progetto, la causa di assenza e la durata in ore della attività progettuale.
+-- SELECT pe.nome, pe.cognome, atp.giorno, pr.nome, a.tipo, atp.oreDurata
+-- FROM Persona pe
+-- JOIN AttivitaProgetto atp ON pe.id = atp.persona
+-- JOIN Progetto pr ON atp.progetto = pr.id
+-- join Assenza a on pe.id = a.persona and atp.giorno = a.giorno
+-- -- 12. Quali sono i WP che hanno lo stesso nome, ma appartengono a progetti diversi?
+-- select WP.nome
+-- from WP
+-- join Progetto pr on pr.id = WP.progetto
+-- where WP.nome =
+
+
--- Esercizio Cielo/Cielo.sql
+++ Esercizio Cielo/Cielo.sql
@@ -21,21 +21,21 @@ create table ArrPart(
     partenza CodiATA not null,
     PRIMARY KEY (codice),
     FOREIGN KEY (codice, comp)references Volo(codice, comp),
-    FOREIGN KEY (arrivo) references Aereoporto(codice),
-    FOREIGN KEY (partenza) references Aereoporto(codice)
+    FOREIGN KEY (arrivo) references Aeroporto(codice),
+    FOREIGN KEY (partenza) references Aeroporto(codice)
 );
-create table Aereoporto(
+create table Aeroporto(
     codice CodiATA not null,
     nome StringaM not null,
     PRIMARY KEY (codice),
-    FOREIGN KEY (codice) references LuogoAereoporto(Aereoporto)
+    FOREIGN KEY (codice) references LuogoAeroporto(Aeroporto)
 );
-create table LuogoAereoporto(
-    aereoporto CodiATA not null,
+create table LuogoAeroporto(
+    Aeroporto CodiATA not null,
     citta StringaM not null,
     nazione StringaM not null,
-    PRIMARY KEY (aereoporto),
-    FOREIGN KEY (aereoporto) references Aereoporto(codice)
+    PRIMARY KEY (Aeroporto),
+    FOREIGN KEY (Aeroporto) references Aeroporto(codice)
 );
 create table Compagnia(
     nome StringaM not null,
@@ -46,72 +46,114 @@ create table Compagnia(
 
 commit
 
--- Quali sono i voli (codice e nome della compagnia) la cui durata supera le 3 ore?
-select V.codice, V.comp
-from Volo v
-where v.durataMinuti >= '180'
--- Quali sono le compagnie che hanno voli che superano le 3 ore?
-select distinct c.nome
-from Compagnia c, Volo v
-where v.durataMinuti > '180'
--- Quali sono i voli (codice e nome della compagnia) che partono dall’aeroporto con
--- codice ‘CIA’ ?
-select distinct ar.comp, ar.codice
-from ArrPart ar
-where ar.partenza = 'CIA'
-
--- Quali sono le compagnie che hanno voli che arrivano all’aeroporto con codice
--- ‘FCO’ ?
-select distinct v.comp
-from volo v, ArrPart ar
-where ar.arrivo = 'FCO'
--- Quali sono i voli (codice e nome della compagnia) che partono dall’aeroporto ‘FCO’
--- e arrivano all’aeroporto ‘JFK’ 
-select distinct ar.comp, ar.codice
-from ArrPart ar
-where ar.partenza = 'FCO'
-    and ar.arrivo = 'JFK'
-
--- Quali sono le compagnie che hanno voli che partono dall’aeroporto ‘FCO’ e atter-
--- rano all’aeroporto ‘JFK’?
-select distinct ar.comp
-from ArrPart ar
-where ar.partenza = 'FCO'
-    and ar.arrivo = 'JFK'
-
--- Quali sono i nomi delle compagnie che hanno voli diretti dalla città di ‘Roma’ alla
--- città di ‘New York’ ?
-select distinct ar.comp
-from ArrPart ar
-where ar.partenza = 'FCO' 
-	or ar.partenza = 'CIA'
-    and ar.arrivo = 'JFK'
--- Quali sono gli aeroporti (con codice IATA, nome e luogo) nei quali partono voli
--- della compagnia di nome ‘MagicFly’ ?
-SELECT a.codice AS codice_iata, a.nome AS nome_aeroporto, la.citta, la.nazione
-FROM ArrPart ap
-JOIN Aeroporto a ON ap.partenza = a.codice
-JOIN Compagnia c ON ap.comp = c.nome
-JOIN LuogoAeroporto la ON a.codice = la.aeroporto
-WHERE c.nome = 'MagicFly';
--- Quali sono i voli che partono da un qualunque aeroporto della città di ‘Roma’ e
--- atterrano ad un qualunque aeroporto della città di ‘New York’ ? Restituire: codice
--- del volo, nome della compagnia, e aeroporti di partenza e arrivo.
-
-SELECT v.codice, c.nome AS compagnia, la1.aeroporto AS partenza, la2.aeroporto AS arrivo
-FROM Volo v
-JOIN ArrPart ap ON v.codice = ap.codice
-JOIN Compagnia c ON v.comp = c.nome
-JOIN LuogoAeroporto la1 ON ap.partenza = la1.aeroporto
-JOIN LuogoAeroporto la2 ON ap.arrivo = la2.aeroporto
-WHERE la1.citta = 'Roma' AND la2.citta = 'New York';
-
-
--- Quali sono le compagnie che hanno voli che partono dall’aeroporto ‘FCO’, atter-
--- rano all’aeroporto ‘JFK’, e di cui si conosce l’anno di fondazione?
-SELECT DISTINCT c.nome
+--CIELO2
+-- 1. Quante sono le compagnie che operano (sia in arrivo che in partenza) nei diversi aeroporti?
+SELECT c.nome AS compagnia, COUNT(DISTINCT a.arrivo) + COUNT(DISTINCT a.partenza) AS numero_aeroporti
 FROM Compagnia c
-JOIN Volo v ON c.nome = v.comp
-JOIN ArrPart ap ON v.codice = ap.codice
-WHERE ap.partenza = 'FCO' AND ap.arrivo = 'JFK';
+JOIN ArrPart a ON c.nome = a.comp
+GROUP BY c.nome;
+-- 2. Quanti sono i voli che partono dall’aeroporto ‘HTR’ e hanno una durata di almeno 100 minuti?
+SELECT COUNT(DISTINCT ar.partenza) as num_voli
+FROM ArrPart ar
+JOIN Volo v on v.codice = ar.codice
+WHERE ar.partenza = 'HTR' and v.durataMinuti >= 100
+-- 3. Quanti sono gli aeroporti sui quali opera la compagnia ‘Apitalia’, per ogni nazione nella quale opera?
+SELECT la.nazione, COUNT(DISTINCT a.codice) AS numero_aeroporti
+FROM Compagnia c
+JOIN ArrPart ap ON c.nome = ap.comp
+JOIN Aeroporto a ON a.codice = ap.arrivo OR a.codice = ap.partenza
+JOIN LuogoAeroporto la ON a.codice = la.aeroporto
+WHERE c.nome = 'Apitalia'
+GROUP BY la.nazione;
+-- 4. Qual è la media, il massimo e il minimo della durata dei voli effettuati dalla compagnia ‘MagicFly’ ?
+select avg(durataMinuti), max(durataMinuti), min(durataMinuti)
+from Volo v
+where v.comp = 'MagicFly'
+-- 5. Qual è l’anno di fondazione della compagnia più vecchia che opera in ognuno degli aeroporti?
+select c.annofondaz
+from Compagnia c, ArrPart ar
+join Aeroporto a on a.codice = ar.partenza and a.codice = ar.arrivo
 
+-- 6. Quante sono le nazioni (diverse) raggiungibili da ogni nazione tramite uno o più voli?
+
+-- 7. Qual è la durata media dei voli che partono da ognuno degli aeroporti?
+-- 8. Qual è la durata complessiva dei voli operati da ognuna delle compagnie fondate a partire dal 1950?
+-- 9. Quali sono gli aeroporti nei quali operano esattamente due compagnie?
+-- 10. Quali sono le città con almeno due aeroporti?
+-- 11. Qual è il nome delle compagnie i cui voli hanno una durata media maggiore di 6 ore?
+-- 12. Qual è il nome delle compagnie i cui voli hanno tutti una durata maggiore di 100 minuti?
+
+
+
+
+
+
+--CIELO 1
+-- -- Quali sono i voli (codice e nome della compagnia) la cui durata supera le 3 ore?
+-- select V.codice, V.comp
+-- from Volo v
+-- where v.durataMinuti >= '180'
+-- -- Quali sono le compagnie che hanno voli che superano le 3 ore?
+-- select distinct c.nome
+-- from Compagnia c, Volo v
+-- where v.durataMinuti > '180'
+-- -- Quali sono i voli (codice e nome della compagnia) che partono dall’aeroporto con
+-- -- codice ‘CIA’ ?
+-- select distinct ar.comp, ar.codice
+-- from ArrPart ar
+-- where ar.partenza = 'CIA'
+
+-- -- Quali sono le compagnie che hanno voli che arrivano all’aeroporto con codice
+-- -- ‘FCO’ ?
+-- select distinct v.comp
+-- from volo v, ArrPart ar
+-- where ar.arrivo = 'FCO'
+-- -- Quali sono i voli (codice e nome della compagnia) che partono dall’aeroporto ‘FCO’
+-- -- e arrivano all’aeroporto ‘JFK’ 
+-- select distinct ar.comp, ar.codice
+-- from ArrPart ar
+-- where ar.partenza = 'FCO'
+--     and ar.arrivo = 'JFK'
+
+-- -- Quali sono le compagnie che hanno voli che partono dall’aeroporto ‘FCO’ e atter-
+-- -- rano all’aeroporto ‘JFK’?
+-- select distinct ar.comp
+-- from ArrPart ar
+-- where ar.partenza = 'FCO'
+--     and ar.arrivo = 'JFK'
+
+-- -- Quali sono i nomi delle compagnie che hanno voli diretti dalla città di ‘Roma’ alla
+-- -- città di ‘New York’ ?
+-- select distinct ar.comp
+-- from ArrPart ar
+-- where ar.partenza = 'FCO' 
+-- 	or ar.partenza = 'CIA'
+--     and ar.arrivo = 'JFK'
+-- -- Quali sono gli aeroporti (con codice IATA, nome e luogo) nei quali partono voli
+-- -- della compagnia di nome ‘MagicFly’ ?
+-- SELECT a.codice AS codice_iata, a.nome AS nome_aeroporto, la.citta, la.nazione
+-- FROM ArrPart ap
+-- JOIN Aeroporto a ON ap.partenza = a.codice
+-- JOIN Compagnia c ON ap.comp = c.nome
+-- JOIN LuogoAeroporto la ON a.codice = la.aeroporto
+-- WHERE c.nome = 'MagicFly';
+-- -- Quali sono i voli che partono da un qualunque aeroporto della città di ‘Roma’ e
+-- -- atterrano ad un qualunque aeroporto della città di ‘New York’ ? Restituire: codice
+-- -- del volo, nome della compagnia, e aeroporti di partenza e arrivo.
+
+-- SELECT v.codice, c.nome AS compagnia, la1.aeroporto AS partenza, la2.aeroporto AS arrivo
+-- FROM Volo v
+-- JOIN ArrPart ap ON v.codice = ap.codice
+-- JOIN Compagnia c ON v.comp = c.nome
+-- JOIN LuogoAeroporto la1 ON ap.partenza = la1.aeroporto
+-- JOIN LuogoAeroporto la2 ON ap.arrivo = la2.aeroporto
+-- WHERE la1.citta = 'Roma' AND la2.citta = 'New York';
+
+
+-- -- Quali sono le compagnie che hanno voli che partono dall’aeroporto ‘FCO’, atter-
+-- -- rano all’aeroporto ‘JFK’, e di cui si conosce l’anno di fondazione?
+-- SELECT DISTINCT c.nome
+-- FROM Compagnia c
+-- JOIN Volo v ON c.nome = v.comp
+-- JOIN ArrPart ap ON v.codice = ap.codice
+-- WHERE ap.partenza = 'FCO' AND ap.arrivo = 'JFK';
